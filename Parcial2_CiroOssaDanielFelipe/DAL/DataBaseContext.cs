d3f6e3d420643e48e8af2d7e29d75f466de57c59
Daniel Ciro <danielciro312@gmail.com>
2023-10-07 23:18:32 -0500

    Db configured and Created with EF Core

---
+++ Parcial2_CiroOssaDanielFelipe/DAL/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_CiroOssaDanielFelipe.DAL.Entities;
+
+namespace Parcial2_CiroOssaDanielFelipe.DAL
+{
+    public class DataBaseContext : DbContext 
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext>options) : base(options)
+        {
+                
+        }
+
+        public DbSet<PersonaNatural> personaNaturals { get; set; }
+    }
+}
